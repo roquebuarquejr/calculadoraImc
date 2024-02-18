ff868794edbc9543a7cc12e0b92cebd255697998
Roque Buarque <roqueb@spotify.com>
2024-02-18 21:29:40 +0000

    clean code

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -43,9 +43,6 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(KEY_RESULT_IMC, resultado)
                 startActivity(intent)
 
-                // Cores
-                // EditText background + Icone
-                // Gradiente + icone + titulo + descricao
             }
         }
     }
